14cbf72c93829b91d060672ec28df4007779c8c0
Anirudh Venkatesh <avenkatesh@fullerton.edu>
2024-11-15 18:23:35 -0800

    Added voting deadline to handle time-sensitive voting

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -19,6 +19,7 @@ contract Ballot {
     }
 
     address public chairperson;
+    uint public deadline; // The deadline for voting (unix timestamp)
 
     // This declares a state variable that
     // stores a Voter struct for each possible address.
@@ -28,17 +29,18 @@ contract Ballot {
     Proposal[] public proposals;
 
     /// Create a new ballot to choose one of proposalNames.
-    constructor(bytes32[] memory proposalNames) {
+    /// The chairperson can also set a deadline (in Unix timestamp).
+    constructor(bytes32[] memory proposalNames, uint votingDurationInSeconds) {
         chairperson = msg.sender;
         voters[chairperson].weight = 1;
+        
+        // Set the voting deadline
+        deadline = block.timestamp + votingDurationInSeconds;
 
         // For each of the provided proposal names,
         // create a new proposal object and add it
         // to the end of the array.
         for (uint i = 0; i < proposalNames.length; i++) {
-            // Proposal({...}) creates a temporary
-            // Proposal object and proposals.push(...)
-            // appends it to the end of proposals.
             proposals.push(Proposal({
                 name: proposalNames[i],
                 voteCount: 0
@@ -46,161 +48,97 @@ contract Ballot {
         }
     }
 
-    // Give voter the right to vote on this ballot.
-    // May only be called by chairperson.
-	/**
-    function giveRightToVote(address voter) external {
-        // If the first argument of require evaluates
-        // to false, execution terminates and all
-        // changes to the state and to Ether balances
-        // are reverted.
-        // This used to consume all gas in old EVM versions, but
-        // not anymore.
-        // It is often a good idea to use require to check if
-        // functions are called correctly.
-        // As a second argument, you can also provide an
-        // explanation about what went wrong.
-        require(
-            msg.sender == chairperson,
-            "Only chairperson can give right to vote."
-        );
-        require(
-            !voters[voter].voted,
-            "The voter already voted."
-        );
-        require(voters[voter].weight == 0);
-        voters[voter].weight = 1;
+    // Function to check if the voting deadline has passed
+    modifier hasNotEnded() {
+        require(block.timestamp <= deadline, "Voting period has ended.");
+        _;
     }
-	*/
-	
-	// Allow the chairperson to give right to vote to multiple voters at once
-	function giveRightsToMultipleVoters(address[] calldata votersList) external {
-		require(msg.sender == chairperson, "Only chairperson can give right to vote.");
+
+    // Allow the chairperson to give right to vote to multiple voters at once
+    function giveRightsToMultipleVoters(address[] calldata votersList) external {
+        require(msg.sender == chairperson, "Only chairperson can give right to vote.");
     
-		for (uint i = 0; i < votersList.length; i++) {
-			address voter = votersList[i];
-			require(!voters[voter].voted, "The voter already voted.");
-			require(voters[voter].weight == 0, "The voter already has voting rights.");
-			voters[voter].weight = 1;
-		}
-	}
+        for (uint i = 0; i < votersList.length; i++) {
+            address voter = votersList[i];
+            require(!voters[voter].voted, "The voter already voted.");
+            require(voters[voter].weight == 0, "The voter already has voting rights.");
+            voters[voter].weight = 1;
+        }
+    }
 
-
-    /// Delegate your vote to the voter to.
-    function delegate(address to) external {
-        // assigns reference
+    // Delegate your vote to another voter
+    function delegate(address to) external hasNotEnded {
         Voter storage sender = voters[msg.sender];
         require(sender.weight != 0, "You have no right to vote");
         require(!sender.voted, "You already voted.");
-
         require(to != msg.sender, "Self-delegation is disallowed.");
 
-        // Forward the delegation as long as
-        // to also delegated.
-        // In general, such loops are very dangerous,
-        // because if they run too long, they might
-        // need more gas than is available in a block.
-        // In this case, the delegation will not be executed,
-        // but in other situations, such loops might
-        // cause a contract to get "stuck" completely.
         while (voters[to].delegate != address(0)) {
             to = voters[to].delegate;
-
-            // We found a loop in the delegation, not allowed.
             require(to != msg.sender, "Found loop in delegation.");
         }
 
         Voter storage delegate_ = voters[to];
+        require(delegate_.weight >= 1, "Delegate cannot vote");
 
-        // Voters cannot delegate to accounts that cannot vote.
-        require(delegate_.weight >= 1);
-
-        // Since sender is a reference, this
-        // modifies voters[msg.sender].
         sender.voted = true;
         sender.delegate = to;
 
         if (delegate_.voted) {
-            // If the delegate already voted,
-            // directly add to the number of votes
             proposals[delegate_.vote].voteCount += sender.weight;
         } else {
-            // If the delegate did not vote yet,
-            // add to her weight.
             delegate_.weight += sender.weight;
         }
     }
 
-    /// Give your vote (including votes delegated to you)
-    /// to proposal proposals[proposal].name.
-    function vote(uint proposal) external {
+    // Cast a vote for a proposal
+    function vote(uint proposal) external hasNotEnded {
         Voter storage sender = voters[msg.sender];
         require(sender.weight != 0, "Has no right to vote");
         require(!sender.voted, "Already voted.");
         sender.voted = true;
         sender.vote = proposal;
 
-        // If proposal is out of the range of the array,
-        // this will throw automatically and revert all
-        // changes.
         proposals[proposal].voteCount += sender.weight;
     }
 
-    /// @dev Computes the winning proposal taking all
-    /// previous votes into account.
-	/**
-    function winningProposal() public view
-            returns (uint winningProposal_)
-    {
+    // Get the index of the winning proposal, accounting for ties
+    function winningProposals() public view returns (uint winningProposal_) {
         uint winningVoteCount = 0;
         for (uint p = 0; p < proposals.length; p++) {
             if (proposals[p].voteCount > winningVoteCount) {
                 winningVoteCount = proposals[p].voteCount;
-                winningProposal_ = p;
             }
         }
-    }
-	*/
-	
-	// Modified function to handle ties by returning a random winning proposal among tied ones
-	function winningProposals() public view
-			returns (uint winningProposal_)
-	{
-    uint winningVoteCount = 0;
 
-    // First, find the highest vote count
-    for (uint p = 0; p < proposals.length; p++) {
-        if (proposals[p].voteCount > winningVoteCount) {
-            winningVoteCount = proposals[p].voteCount;
+        uint[] memory tiedProposals = new uint[](proposals.length);
+        uint count = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount == winningVoteCount) {
+                tiedProposals[count] = p;
+                count++;
+            }
+        }
+
+        if (count > 1) {
+            uint randomIndex = uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % count;
+            winningProposal_ = tiedProposals[randomIndex];
+        } else {
+            winningProposal_ = tiedProposals[0];
         }
     }
 
-    // Second, collect all proposals with the highest vote count
-    uint[] memory tiedProposals = new uint[](proposals.length);
-    uint count = 0;
-    for (uint p = 0; p < proposals.length; p++) {
-        if (proposals[p].voteCount == winningVoteCount) {
-            tiedProposals[count] = p;
-            count++;
+    // Get the name of the winning proposal
+    function winnerName() external view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposals()].name;
+    }
+
+    // Get the remaining time until the voting ends (in seconds)
+    function remainingTime() external view returns (uint) {
+        if (block.timestamp >= deadline) {
+            return 0;
+        } else {
+            return deadline - block.timestamp;
         }
-    }
-
-    // If there is more than one tied proposal, select a random one
-    if (count > 1) {
-        uint randomIndex = uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % count;
-        winningProposal_ = tiedProposals[randomIndex];
-    } else {
-        // No tie, just return the single winner
-        winningProposal_ = tiedProposals[0];
-    }
-	}
-
-    // Calls winningProposal() function to get the index
-    // of the winner contained in the proposals array and then
-    // returns the name of the winner
-    function winnerName() external view
-            returns (bytes32 winnerName_)
-    {
-        winnerName_ = proposals[winningProposal()].name;
     }
 }
