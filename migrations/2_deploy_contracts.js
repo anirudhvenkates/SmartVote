af3257b474ddaeb734dd908d356ce623498a5f06
Anirudh Venkatesh <avenkatesh@fullerton.edu>
2024-11-16 16:31:10 -0800

    Added parameters to deploy ballot contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,12 @@
 var ballot = artifacts.require("./ballot.sol");
 
 module.exports = function(deployer) {
+	// Define the proposal names as an array of bytes32
+	const proposalNames = ["Proposal 1", "Proposal 2", "Proposal 3"].map(name => web3.utils.asciiToHex(name));
 
-	deployer.deploy(ballot);
+	// Define the voting duration in seconds (e.g., 1 day)
+	const votingDurationInSeconds = 24 * 60 * 60;  // 24 hours in seconds
+
+	// Deploy the Ballot contract with the necessary parameters
+	deployer.deploy(ballot, proposalNames, votingDurationInSeconds);
 }
