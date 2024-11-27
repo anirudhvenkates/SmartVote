c8a71c6027eb64fa4ea89abad120d03215430653
Anirudh Venkatesh <avenkatesh@fullerton.edu>
2024-11-26 16:35:11 -0800

    Revert "Modified contract deployment"
    This reverts commit da0a941f11b384f0605ac08da29f938b4ec94abd.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,14 @@ var Ballot = artifacts.require("./Ballot.sol");
 var Voting = artifacts.require("./Voting.sol");
 
 module.exports = function(deployer) {
+  // Define the proposal names as an array of bytes32
+  const proposalNames = ["Proposal 1", "Proposal 2", "Proposal 3"].map(name => web3.utils.asciiToHex(name));
+
+  // Define the voting duration in seconds (e.g., 1 day)
+  const votingDurationInSeconds = 24 * 60 * 60;  // 24 hours in seconds
 
   // Deploy the Ballot contract with the necessary parameters
-  deployer.deploy(Ballot)
+  deployer.deploy(Ballot, proposalNames, votingDurationInSeconds)
     .then(function() {
       // After the Ballot contract is deployed, get its address
       const ballotAddress = Ballot.address;
