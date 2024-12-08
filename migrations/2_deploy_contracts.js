6aebec4a07e7b28893e40cbed978372080fa9a09
Anirudh Venkatesh <avenkatesh@fullerton.edu>
2024-12-08 15:05:14 -0800

    Chairperson address now initialized directly from wallet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function(deployer, network, accounts) {
-    // Deploy the BallotManager contract first
-    deployer.deploy(BallotManager).then(async (ballotManagerInstance) => {
+    // Hardcode the chairperson address here
+    const chairpersonAddress = accounts[0]; // Use the first account in Ganache for the chairperson
+
+    // Deploy the BallotManager contract and pass the chairperson address
+    deployer.deploy(BallotManager, chairpersonAddress).then(async (ballotManagerInstance) => {
 
         // Define the proposal names as an array of bytes32
         const proposalNames = ["Proposal 1", "Proposal 2", "Proposal 3"].map(name => web3.utils.asciiToHex(name));
-
-        // Hardcode the chairperson address here
-        const chairpersonAddress = accounts[0]; // Use the first account in Ganache for the chairperson
 
         // Define the voting duration in seconds (e.g., 10 minutes for testing)
         const votingDurationInSeconds = 10 * 60; // 10 minutes in seconds
@@ -21,11 +21,10 @@ module.exports = function(deployer, network, accounts) {
 
         // Optionally, if you want to confirm the Ballot address
         const ballotAddress = await ballotManagerInstance.getBallotAddress(0);
-        console.log("Deployed BallotManager contract address:", ballotManagerInstance.address);
+        console.log("Deployed Ballot contract address:", ballotAddress);
 
         // Save the BallotManager contract address to a JSON file
-        //const contractAddressPath = path.join(__dirname, '../public/contract_address.json');
-		const contractAddressPath = path.join(process.cwd(), 'public', 'contract_address.json');
+        const contractAddressPath = path.join(process.cwd(), 'public', 'contract_address.json');
         const contractAddressData = JSON.stringify({ contractAddress: ballotManagerInstance.address }, null, 2);
         
         fs.writeFileSync(contractAddressPath, contractAddressData);
